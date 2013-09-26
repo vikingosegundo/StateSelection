45bc98a32183f2924b8c580df3d44095e2dceea3
Manuel Meyer <manuel@vikingosegundo.de>
2013-09-26 20:46:01 +0200

    Added filtering for states

--- StateSelection/StateSelctionViewController.m
+++ StateSelection/StateSelctionViewController.m
@@ -13,8 +13,8 @@
 static NSDictionary *stateDictionary;
 static NSString *CellIdentifier = @"MyStateCell";
 
-@interface StateSelctionViewController ()
-
+@interface StateSelctionViewController ()<UISearchBarDelegate>
+@property(nonatomic, strong) NSArray *filteredStates;
 @end
 
 @implementation StateSelctionViewController
@@ -32,7 +32,9 @@ static NSString *CellIdentifier = @"MyStateCell";
 {
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
-
+    [self.searchDisplayController.searchResultsTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
+    //self.tableView.tableHeaderView = self.searchDisplayController.searchBar;
+    self.filteredStates = @[];
     if (!stateDictionary) {
         stateDictionary = @{@"United States":@[@"Alabama", @"Alaska", @"Arizona",
                                                @"Arkansas", @"California", @"Colorado",
@@ -64,24 +66,40 @@ static NSString *CellIdentifier = @"MyStateCell";
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    if(self.searchDisplayController.searchResultsTableView == tableView)
+        return [self.filteredStates count];
     return [stateDictionary[_selectedNation] count];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    cell.textLabel.text = stateDictionary[_selectedNation][indexPath.row];
+    if(tableView == self.tableView)
+        cell.textLabel.text = stateDictionary[_selectedNation][indexPath.row];
+    else
+        cell.textLabel.text = _filteredStates[indexPath.row];
     return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [self.delegate selectedState:stateDictionary[_selectedNation][indexPath.row] forNation:_selectedNation];
+    if(tableView == self.tableView)
+        [self.delegate selectedState:stateDictionary[_selectedNation][indexPath.row] forNation:_selectedNation];
+    else
+        [self.delegate selectedState:_filteredStates[indexPath.row] forNation:_selectedNation];
     if(self.presentingViewController)
         [self dismissViewControllerAnimated:NO completion:NULL];
     else
         [self.navigationController popViewControllerAnimated:YES];
     
 }
+
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    self.filteredStates = [stateDictionary[_selectedNation] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self BEGINSWITH[c] %@", searchText]];
+    [self.searchDisplayController.searchResultsTableView reloadData];
+}
+
 
 @end
